75a013fc4da40525a331f7df91a5bb481be45079
llaeah <alleahjane648@gmail.com>
2026-04-21 01:10:50 +0800

    aus

--- app/src/main/java/com/example/shareninsulares/Notification.java
+++ app/src/main/java/com/example/shareninsulares/Notification.java
@@ -2,35 +2,19 @@ package com.example.shareninsulares;
 
 import android.os.Bundle;
 
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterForm extends AppCompatActivity {
+public class Notification extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register_form);
-
-        String[] campusOptions = {"Main Campus", "Balanga Campus"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                campusOptions
-        );
-
-        AutoCompleteTextView campusDropdown = findViewById(R.id.campus_dropdown);
-        if (campusDropdown != null) {
-            campusDropdown.setAdapter(adapter);
-        }
-
+        setContentView(R.layout.activity_notification);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/shareninsulares/Post.java
+++ app/src/main/java/com/example/shareninsulares/Post.java
@@ -2,6 +2,8 @@ package com.example.shareninsulares;
 
 import android.os.Bundle;
 
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,6 +17,28 @@ public class Post extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_post);
+
+        String[] campusOptions = {"Main Campus", "Balanga Campus"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                campusOptions
+        );
+
+        AutoCompleteTextView locationDropdown = findViewById(R.id.locationDropdown);
+        if (locationDropdown != null) {
+            locationDropdown.setAdapter(adapter);
+        }
+
+        String[] categoryOptions = {"Buy/Sell", "Rent"};
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, categoryOptions);
+
+        AutoCompleteTextView categoryDropdown = findViewById(R.id.categoryDropdown);
+        if (categoryDropdown != null) {
+            categoryDropdown.setAdapter(categoryAdapter);
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
